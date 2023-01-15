1bb3458d973c67a57161d2d4a2a12f5e1264e8a3
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2023-01-15 19:12:57 -0300

    inicio backend

---
+++ portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/PortfolioFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.portfolioFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioFullStackApplication.class, args);
+	}
+
+}
---
+++ portfolioFullStack/src/test/java/com/miPortfolio/portfolioFullStack/PortfolioFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.portfolioFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
