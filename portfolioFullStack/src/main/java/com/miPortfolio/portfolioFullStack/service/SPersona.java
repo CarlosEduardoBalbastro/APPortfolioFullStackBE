7ecb2cacb91bb97f12e641148cc1c8cd2aaf65e9
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2023-01-17 16:42:27 -0300

    continuando desarrollo BE

--- portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/service/SPersona.java
+++ portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/service/SPersona.java
@@ -1,8 +1,10 @@
 
 package com.miPortfolio.portfolioFullStack.service;
 
+import com.miPortfolio.portfolioFullStack.model.Persona;
 import com.miPortfolio.portfolioFullStack.repository.RPersona;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,26 @@ public class SPersona {
     
     @Autowired
     private RPersona personRepository;
+    
+    public List<Persona> getPersonas(){
+        List<Persona> listaPersonas = personRepository.findAll();
+        return listaPersonas;
+    }
+    
+    
+    public void savePersona(Persona perso){
+        personRepository.save(perso);
+    }
+    
+    
+    public void deletePersona(int id){
+        personRepository.deleteById(id);
+    }
+    
+    public Persona findPersona(int id){
+        Persona perso = personRepository.findById(id).orElse(null);
+        return perso;
+    }
+    
     
 }
