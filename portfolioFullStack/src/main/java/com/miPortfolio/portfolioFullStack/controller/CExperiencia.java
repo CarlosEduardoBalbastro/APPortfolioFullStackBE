24f10e7bc8a1a36be671b0df827d736c5b35f026
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2023-04-27 21:57:43 -0300

    modificacion Deploy

--- portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/controller/CExperiencia.java
+++ portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/controller/CExperiencia.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/experiencias") 
-@CrossOrigin ( origins = "http://localhost:4200" )
+//@CrossOrigin ( origins = "http://localhost:4200" )
 public class CExperiencia {
     
 
--- portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/controller/CPersona.java
+++ portfolioFullStack/src/main/java/com/miPortfolio/portfolioFullStack/controller/CPersona.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")//localhost:4200
-@CrossOrigin( origins = "http://localhost:4200")
+//@CrossOrigin( origins = "http://localhost:4200")
 public class CPersona {
     
     @Autowired
